3e611c331e77af2eb30fadb135daac6151f7b3f2
Owen Ng <nigel.ngyanhing@mail.utoronto.ca>
2021-06-13 15:17:49 -0400

    Initial

---
+++ process.py
@@ -0,0 +1,4 @@
+import subprocess
+subprocess.run(["git", "status"])
+# CompletedProcess(args=['C:\\Program Files\\Git\\bin\\bash.exe', '-c', 'ls'], returncode=0,
+#                  stdout=b'README.md\n__pycache__\nconda_create.sh\nenvs\nmain.py\ntest.sh\nzipped\n')
