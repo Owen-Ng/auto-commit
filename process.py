f166a28a2513f482d566aeb78752b89d88b1b00c
Owen Ng <nigel.ngyanhing@mail.utoronto.ca>
2021-06-13 15:35:06 -0400

    'ew'

--- process.py
+++ process.py
@@ -1,4 +1,10 @@
 import subprocess
-subprocess.run(["git", "status"])
+f = open("./file.txt", "a")
+f.write(" ")
+f.close()
+subprocess.run(["git", "status", ";"])
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "'ew'"])
+subprocess.run(["git", "push"])
 # CompletedProcess(args=['C:\\Program Files\\Git\\bin\\bash.exe', '-c', 'ls'], returncode=0,
 #                  stdout=b'README.md\n__pycache__\nconda_create.sh\nenvs\nmain.py\ntest.sh\nzipped\n')
