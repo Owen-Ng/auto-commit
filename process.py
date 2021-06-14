3cd7107441fdba1e6d0b43946a2fbabf0f221a23
OwenNg <>
2021-06-14 17:24:01 +0000

    'Daily commit'

--- process.py
+++ process.py
@@ -7,7 +7,7 @@ def execute():
     f.close()
     subprocess.run(["git", "status", ";"])
     subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", "'ew'"])
+    subprocess.run(["git", "commit", "-m", "'Daily commit'"])
     subprocess.run(["git", "push"])
 # CompletedProcess(args=['C:\\Program Files\\Git\\bin\\bash.exe', '-c', 'ls'], returncode=0,
 #                  stdout=b'README.md\n__pycache__\nconda_create.sh\nenvs\nmain.py\ntest.sh\nzipped\n')
--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,9 @@ from process import execute
 import time
 from continuous import continuous
 
-# schedule.every().day.at("13:00").do(execute)
+schedule.every().day.at("17:24").do(execute)
 
-schedule.every(10).seconds.do(execute)
+# schedule.every(10).seconds.do(execute)
 
 continuous()
 while True:
