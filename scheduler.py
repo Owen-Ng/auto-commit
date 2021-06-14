b32ca5f398ef6eac05c0fcf60036f03b8298ae1e
OwenNg <>
2021-06-14 07:11:41 +0000

    'ew'

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,13 @@
 import schedule
 from process import execute
 import time
+from continuous import continuous
 
-schedule.every().every().day.at("13:00").do(execute)
+# schedule.every().day.at("13:00").do(execute)
 
+schedule.every(10).seconds.do(execute)
+
+continuous()
 while True:
     schedule.run_pending()
     time.sleep(1)
