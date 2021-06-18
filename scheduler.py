316f6552a3984a2a82f726f714bef6624db8a465
OwenNg <>
2021-06-18 18:50:36 +0000

    Test

--- scheduler.py
+++ scheduler.py
@@ -2,9 +2,12 @@ import schedule
 from process import execute
 import time
 from continuous import continuous
+from keep import keep
 
 # schedule.every().day.at("17:24").do(execute)
+schedule.every().hour.do(keep)
 schedule.every(2).hours.do(execute)
+
 # schedule.every(10).seconds.do(execute)
 
 continuous()
